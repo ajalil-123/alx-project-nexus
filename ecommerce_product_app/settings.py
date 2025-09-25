89a9533cb4a5eca47b18b02349cde85c2c280c98
abdul jalil <abduljalilzakaria1@gmail.com>
2025-09-25 19:42:43 +0200

    added configuration to deploy on render

--- ecommerce_product_app/settings.py
+++ ecommerce_product_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kq=ufcgoc-9s3%6g+56_%k=v&!!z^@cuo24o)z61tnuoejmbas
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -49,6 +49,7 @@ AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -57,6 +58,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
 
 # Django REST Framework configuration
 REST_FRAMEWORK = {
